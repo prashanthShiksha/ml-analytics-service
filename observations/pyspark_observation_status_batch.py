489cbeeaacb7a99dbfaa45ee9dab975365f89714
Snehangsu De <63997773+snehangsude@users.noreply.github.com>
2023-03-29 15:39:47 +0530

    Update pyspark_observation_status_batch.py
    Change timedelta value to 23:59 hrs

--- observations/pyspark_observation_status_batch.py
+++ observations/pyspark_observation_status_batch.py
@@ -69,7 +69,7 @@ for tasks in last_ingestion:
     if tasks['type'] == 'index':
         last_timestamp = dateutil.parser.parse((tasks['createdTime'])).date()
 
-cur_timestamp = (datetime.datetime.now() - datetime.timedelta(1)).date()
+cur_timestamp = (datetime.datetime.now() - datetime.timedelta(hours=23, minutes=59)).date()
 if cur_timestamp == last_timestamp:
     bot.api_call("chat.postMessage",channel=config.get("SLACK","channel"),text=f"TERMINATED: Duplicate Run for observation.")
     sys.exit()
